4dcf4974329c82e3347d359bb1eafa837b088885
Marc Huang <marc.huang@logicmonitor.com>
2017-01-22 17:17:45 +0800

    initial project

---
+++ src/test/java/com/github/marcnuth/TestMain.java
@@ -0,0 +1,11 @@
+package com.github.marcnuth;
+
+import org.junit.Test;
+
+public class TestMain {
+
+    @Test
+    public void testMain() throws Exception {
+        System.out.print("124");
+    }
+}
